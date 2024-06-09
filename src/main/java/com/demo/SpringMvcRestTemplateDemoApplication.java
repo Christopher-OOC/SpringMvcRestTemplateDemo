a5368ea8162815711ec325e1357bbe2b5023698d
Christopher-OOC <olojedechristopher24@gmail.com>
2024-06-09 23:52:54 +0100

    commit

--- src/main/java/com/demo/SpringMvcRestTemplateDemoApplication.java
+++ src/main/java/com/demo/SpringMvcRestTemplateDemoApplication.java
@@ -2,12 +2,20 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class SpringMvcRestTemplateDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcRestTemplateDemoApplication.class, args);
+	}
+	
+	@Bean
+	ObjectMapper getObjectMapper() {
+		return new ObjectMapper();
 	}
 
 }
